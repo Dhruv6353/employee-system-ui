cee8d027d8ad3b84b0610041b4ff4750303b2604
Dhruv6353 <85423663+Dhruv6353@users.noreply.github.com>
2022-07-17 18:15:53 +0530

    my first commit react website

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="bg-gray-800">
+    <div className="h-16 px-8 flex item-center py-4">
+      <p className='text-white font-bold'>Employee Management System</p>
+    </div>
+    </div>
+  );
+};
+
+export default Navbar
